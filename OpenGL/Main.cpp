cda3995927d7aa5aab33c69526eec1ba5b8660ee
Ballai <ballaifoktjeno@gmail.com>
2025-01-20 07:31:08 +0200

    Update Main.cpp

--- OpenGL/Main.cpp
+++ OpenGL/Main.cpp
@@ -6,11 +6,8 @@ namespace fs = std::filesystem;
 #include<math.h>
 #include"Model.h"
 
-
 const unsigned int width = 800;
 const unsigned int height = 800;
-
-
 
 float skyboxVertices[] =
 {
@@ -52,40 +49,27 @@ float randf()
 	return -1.0f + (rand() / (RAND_MAX / 2.0f));
 }
 
+
+
 int main()
 {
-	// Initialize GLFW
 	glfwInit();
-
 	// Tell GLFW what version of OpenGL we are using 
-	// In this case we are using OpenGL 3.3
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	// Tell GLFW we are using the CORE profile
-	// So that means we only have the modern functions
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	// Create a GLFWwindow object of 800 by 800 pixels, naming it "YoutubeOpenGL"
-	GLFWwindow* window = glfwCreateWindow(width, height, "YoutubeOpenGL", NULL, NULL);
-	// Error check if the window fails to create
+	GLFWwindow* window = glfwCreateWindow(width, height, "OpenGL", NULL, NULL);
 	if (window == NULL)
 	{
 		std::cout << "Failed to create GLFW window" << std::endl;
 		glfwTerminate();
 		return -1;
 	}
-	// Introduce the window into the current context
 	glfwMakeContextCurrent(window);
 
-	//Load GLAD so it configures OpenGL
 	gladLoadGL();
-	// Specify the viewport of OpenGL in the Window
-	// In this case the viewport goes from x = 0, y = 0, to x = 800, y = 800
 	glViewport(0, 0, width, height);
-
-
-
-
 
 	// Generates Shader objects
 	Shader shaderProgram("default.vert", "default.frag");
@@ -105,13 +89,8 @@ int main()
 	glUniform4f(glGetUniformLocation(asteroidShader.ID, "lightColor"), lightColor.x, lightColor.y, lightColor.z, lightColor.w);
 	glUniform3f(glGetUniformLocation(asteroidShader.ID, "lightPos"), lightPos.x, lightPos.y, lightPos.z);
 
-
-
-	
-
 	// Enables the Depth Buffer
 	glEnable(GL_DEPTH_TEST);
-
 	// Enables Cull Facing
 	glEnable(GL_CULL_FACE);
 	// Keeps front faces
@@ -122,13 +101,6 @@ int main()
 	// Creates camera object
 	Camera camera(width, height, glm::vec3(0.0f, 0.0f, 2.0f));
 
-
-	/*
-	* I'm doing this relative path thing in order to centralize all the resources into one folder and not
-	* duplicate them between tutorial folders. You can just copy paste the resources from the 'Resources'
-	* folder and then give a relative path from this folder to whatever resource you want to get to.
-	* Also note that this requires C++17, so go to Project Properties, C/C++, Language, and select C++17
-	*/
 	std::string parentDir = (fs::current_path().fs::path::parent_path()).string();
 	std::string jupiterPath = "/Resources/YoutubeOpenGL 21 - Instancing/models/jupiter/scene.gltf";
 	std::string asteroidPath = "/Resources/YoutubeOpenGL 21 - Instancing/models/asteroid/scene.gltf";
@@ -136,18 +108,11 @@ int main()
 	// Load in models
 	Model jupiter((parentDir + jupiterPath).c_str());
 
-
-
-	// Variables to create periodic event for FPS displaying
+	// FPS Counter
 	double prevTime = 0.0;
 	double crntTime = 0.0;
 	double timeDiff;
-	// Keeps track of the amount of frames in timeDiff
 	unsigned int counter = 0;
-
-	// Use this to disable VSync (not advized)
-	//glfwSwapInterval(0);
-
 
 	// Create VAO, VBO, and EBO for the skybox
 	unsigned int skyboxVAO, skyboxVBO, skyboxEBO;
@@ -164,7 +129,6 @@ int main()
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-
 
 	// All the faces of the cubemap (make sure they are in this exact order)
 	std::string facesCubemap[6] =
@@ -187,8 +151,6 @@ int main()
 	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
 	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
-	// This might help with seams on some systems
-	//glEnable(GL_TEXTURE_CUBE_MAP_SEAMLESS);
 
 	// Cycles through all the textures and attaches them to the cubemap object
 	for (unsigned int i = 0; i < 6; i++)
@@ -218,7 +180,6 @@ int main()
 			stbi_image_free(data);
 		}
 	}
-
 
 	// The number of asteroids to be created
 	const unsigned int number = 5000;
@@ -276,6 +237,7 @@ int main()
 	Model asteroid((parentDir + asteroidPath).c_str(), number, instanceMatrix);
 
 	float jupiterRotation = 0.0f;
+	
 	// Main while loop
 	while (!glfwWindowShouldClose(window))
 	{
@@ -289,7 +251,7 @@ int main()
 			// Creates new title
 			std::string FPS = std::to_string((1.0 / timeDiff) * counter);
 			std::string ms = std::to_string((timeDiff / counter) * 1000);
-			std::string newTitle = "YoutubeOpenGL - " + FPS + "FPS / " + ms + "ms";
+			std::string newTitle = "OpenGL - " + FPS + "FPS / " + ms + "ms";
 			glfwSetWindowTitle(window, newTitle.c_str());
 
 			// Resets times and counter
@@ -322,7 +284,6 @@ int main()
 		glm::mat4 view = glm::mat4(1.0f);
 		glm::mat4 projection = glm::mat4(1.0f);
 		// We make the mat4 into a mat3 and then a mat4 again in order to get rid of the last row and column
-		// The last row and column affect the translation of the skybox (which we don't want to affect)
 		view = glm::mat4(glm::mat3(glm::lookAt(camera.Position, camera.Position + camera.Orientation, camera.Up)));
 		projection = glm::perspective(glm::radians(45.0f), (float)width / height, 0.1f, 100.0f);
 		glUniformMatrix4fv(glGetUniformLocation(skyboxShader.ID, "view"), 1, GL_FALSE, glm::value_ptr(view));
@@ -338,22 +299,16 @@ int main()
 
 		// Switch back to the normal depth function
 		glDepthFunc(GL_LESS);
-
-
 		// Swap the back buffer with the front buffer
 		glfwSwapBuffers(window);
 		// Take care of all GLFW events
 		glfwPollEvents();
 	}
 
-
-
 	// Delete all the objects we've created
 	shaderProgram.Delete();
 	skyboxShader.Delete();
-	// Delete window before ending the program
 	glfwDestroyWindow(window);
-	// Terminate GLFW before ending the program
 	glfwTerminate();
 	return 0;
 }
